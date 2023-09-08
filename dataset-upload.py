302e6ae7f4a26310e3976f3c4e238a487bd24a3f
Ackey-code <1986ackey@gmail.com>
2023-09-08 14:40:30 +0900

    Preparing the dataset. Mistakenly disclosed API key has been removed.

---
+++ dataset-upload.py
@@ -0,0 +1,8 @@
+import os
+import openai
+openai.api_key = os.getenv("OPENAI_API_KEY")
+response = openai.File.create(
+  file=open("dataset/gpt3-5turbo-dataset.jsonl", "rb"),
+  purpose='fine-tune'
+)
+print(response)
