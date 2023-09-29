0d79170851d818e62649fcf72eb732f9a7a7f703
Ackey-code <1986ackey@gmail.com>
2023-09-30 02:17:17 +0900

    add size info in prompt

--- src/make-gpt3-5turbo-dataset.py
+++ src/make-gpt3-5turbo-dataset.py
@@ -7,15 +7,15 @@ import numpy as np
 import jsonlines
 import matplotlib.pyplot as plt
 import csv
-CSV_FILE_PATH = './evogym-design-tool/exported/caption.csv'
-JSONL_FILE_PATH = 'dataset/gpt3-5turbo-dataset-v2.jsonl'
+CSV_FILE_PATH = './evogym-design-tool/exported/caption-v2.csv'
+JSONL_FILE_PATH = './dataset/gpt3-5turbo-dataset-v2.jsonl'
 
 def get_caption_by_index(CSV_FILE_PATH, target_index):
     with open(CSV_FILE_PATH, 'r', encoding='utf-8') as csv_file:
         csv_reader = csv.DictReader(csv_file)
         for idx, row in enumerate(csv_reader, start=1):
             if idx == target_index:
-                caption_en = row['caption-en']
+                caption_en = row['caption-en-v2']
                 return caption_en
     return None  
 
@@ -51,7 +51,8 @@ for i in range(1, 101):  # a1.json から a100.json までループ
         Text.append(t)
 
     caption = get_caption_by_index(CSV_FILE_PATH, index)
-    system_text = "The environment in reinforcement learning, where the agent progresses from left to right, is represented by a list of strings, one character per block. The following conditions are applied: '-' is a blank block, 'H' is a hard block, and 'S' is a soft block. The agent can walk on the 'H' and 'S' blocks and can exist in the '-' area. If there is no 'H' or 'S' block under the agent, it will fall. Strings in [0]~[n] of the list must have the same number of characters, and characters other than '-', 'H', and 'S' are not allowed. Return a list that predicts what kind of environment it is from a prompt that describes the given environment. Do not output anything other than a list."
+    caption = f"{width}*{height} size " + caption
+    system_text = "The environment in reinforcement learning, where the agent progresses from left to right, is represented by a list of strings, one character per block. The following conditions are applied: '-' is a blank block, 'H' is a hard block, and 'S' is a soft block. The agent can walk on the 'H' and 'S' blocks and can exist in the '-' area. If there is no 'H' or 'S' block under the agent, it will fall. Please return a list that predicts what kind of environment it is from a prompt that describes the given environment. Please make all elements in the list have the same length. Also, only allow '-' , 'H', and 'S' characters in the elements. Please return with the specified character length. Do not output anything other than a list."
 
     # JSONLファイルに保存するためのデータ作成
     data = {
@@ -64,7 +65,7 @@ for i in range(1, 101):  # a1.json から a100.json までループ
     with jsonlines.open(JSONL_FILE_PATH, mode='a') as writer:  # 'a'モードで追記
         writer.write(data)
 
-    print(f'データがJSONLファイルに追加されました。')
+print(f"データが{JSONL_FILE_PATH}に追加されました。")
 
 
 
