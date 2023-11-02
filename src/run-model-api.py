c680d7f798327054cc9737f808537563bbd4725f
Ackey-code <1986ackey@gmail.com>
2023-11-03 03:50:32 +0900

    to json file

--- src/run-model-api.py
+++ src/run-model-api.py
@@ -1,5 +1,6 @@
 import openai
 import os
+import json
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 def create_env(prompt):
@@ -45,6 +46,58 @@ def check_columns(lst):
 
     return True
 
+def process_neighbour(i, j, grid, object_cells):
+    if i<0 or j<0 or i>=len(grid) or j>=len(grid[0]) or grid[i][j] == "-":
+        return
+
+    cell = (len(grid)-1-i)*len(grid[0]) + j  # fixed here
+
+    if cell in object_cells:
+        return
+
+    object_cells.append(cell)
+
+    process_neighbour(i+1, j, grid, object_cells)
+    process_neighbour(i-1, j, grid, object_cells)
+    process_neighbour(i, j+1, grid, object_cells)
+    process_neighbour(i, j-1, grid, object_cells)
+
+
+def create_json_file(env_list):
+    grid = [list(row) for row in env_list]
+    objects_dict = dict()
+    object_counter = 1
+
+    grid_width = len(grid[0])
+    grid_height = len(grid)
+
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 'H' or grid[i][j] == 'S':
+                new_object_indices = []
+
+                process_neighbour(i, j, grid, new_object_indices)
+
+                new_object_types = [1 if grid[(len(grid)-1-idx//grid_width)][idx%grid_width] == 'H' else 2 for idx in new_object_indices]
+
+                new_object = {}
+                new_object['indices'] = new_object_indices
+                new_object['types'] = new_object_types
+                new_object['neighbors'] = {str(idx): [n for n in new_object_indices if (n//grid_width == idx//grid_width and abs(n%grid_width - idx%grid_width) == 1) or (abs(n//grid_width - idx//grid_width) == 1 and n%grid_width == idx%grid_width)] for idx in new_object_indices}
+
+                objects_dict['new_object_' + str(object_counter)] = new_object
+                object_counter += 1 
+
+                for idx in new_object_indices:
+                    grid[(len(grid)-1-idx//grid_width)][idx%grid_width] = "-"  # fixed here
+
+    return_dict = {}
+    return_dict['grid_width'] = grid_width
+    return_dict['grid_height'] = grid_height
+    return_dict['objects'] = objects_dict
+
+    return return_dict
+
 def generate_env(prompt):
     checked_list = False
 
@@ -52,14 +105,21 @@ def generate_env(prompt):
         env_list = create_env(prompt)
         fixed_list = adjust_list(env_list)
         checked_list = check_columns(fixed_list)
+        try:
+            json_env = create_json_file(fixed_list)
+        except:
+            checked_list = False
 
-    return fixed_list
+    return json_env, fixed_list
 
 def main():
     prompt = "100*20 size Evolution Gym environment that is shaped like a mountain."
-    fixed_list = generate_env(prompt)
+    json_env, fixed_list = generate_env(prompt)
     for s in fixed_list:
         print(s)
+    print(json_env)
+    with open('test.json', 'w') as f:
+        json.dump(json_env, f)
 
 if __name__ == "__main__":
     main()
